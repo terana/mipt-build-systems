90b168406c6f473d57faf9cf9cd40df646f38c80
Anastasiia Terenteva <terenteva8412@gmail.com>
2020-11-28 16:43:28 +0100

    Polishing project. - Fixed root CMakeLists.txt discrepancies, added best-practices. - Formatting fixes. - Keeping doc directory in project.

--- c/preparing.py
+++ c/preparing.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
 if __name__ == '__main__':
-    project_dir = sys.argv[1]
-    output_path = os.path.join(project_dir, "c", "index.h")
-
-    with open(output_path, "w") as f:
+    with open("index.h", "w") as f:
         f.write("const int kCMagicConst = 228;")
